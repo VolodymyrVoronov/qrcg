e3f4406d835d396379dd9537f003c2b3b69f07e4
Volodymyr Voronov <malones.vladimir@gmail.com>
2022-08-31 18:35:14 +0200

    chore(components): deleted ColorInput.

--- src/QrGenerator/QrGenerator.tsx
+++ src/QrGenerator/QrGenerator.tsx
@@ -1,26 +1,15 @@
 import React, { ChangeEvent } from "react";
 import QRCode from "react-qr-code";
 import { motion } from "framer-motion";
-import { ColorResult } from "react-color";
 
 import useQrGeneratorStore from "../state/qrGenerator";
 
 import Input from "../components/Input/Input";
-import ColorInput from "../components/ColorInput/ColorInput";
 
 import styles from "./QrGenerator.module.css";
 
 const QrGenerator = (): JSX.Element => {
-  const { value, size, bgColor, fgColor, setSize, setBgColor, setFgColor } =
-    useQrGeneratorStore();
-
-  const onColorBgChange = (color: ColorResult): void => {
-    setBgColor(color.hex);
-  };
-
-  const onColorFgChange = (color: ColorResult) => {
-    setFgColor(color.hex);
-  };
+  const { value, size, setSize } = useQrGeneratorStore();
 
   return (
     <motion.div
@@ -31,25 +20,13 @@ const QrGenerator = (): JSX.Element => {
     >
       <div className={styles.qrGeneratorSettingBox}>
         <Input />
-
-        <ColorInput
-          text="Background color"
-          onInputChange={onColorBgChange}
-          activeColor={bgColor}
-        />
-
-        <ColorInput
-          text="QR Code color"
-          onInputChange={onColorFgChange}
-          activeColor={fgColor}
-        />
       </div>
 
       <div
         className={styles.qrGeneratorCodeBox}
         style={{ width: `${size}` + "px" }}
       >
-        <QRCode value={value} size={size} bgColor={bgColor} fgColor={fgColor} />
+        <QRCode value={value} size={size} />
       </div>
     </motion.div>
   );
